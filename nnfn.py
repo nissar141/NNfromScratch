725328353d8fc6f3a9eb7a6ea9ca8709e14e1b4a
Nissar Ali <nissar.nit@gmail.com>
2025-06-16 11:14:33 +0530

    Debugged the array indices related mistakes

--- nnfn.py
+++ nnfn.py
@@ -9,24 +9,36 @@ def forwardPass(X, W, b):
         A[i+1] = reLU(Z[i+1]) if i < len(W)-1 else sigmoid(Z[i+1])
     return Z, A
 
-def backwardPass(A, Y, X, Z):
+def backwardPass(W, A, Y, Z):
     m = A[0].shape[1]
+    L = len(A) - 1
     dZ, dW, db = {}, {}, {} 
-    dZ[len(A)] = A[-1]-Y
-    for i in reversed(range(len(A))):
-        dW[i+1] = (A[] @ dZ[].T)/m
-        db[i+1] = np.mean(dZ,axis=1,keepdims=True)
-        dZ[i] = np.dot(dZ[i+1],dW[i+1],reLU(Z[i]))
+    dZ[L] = A[L] - Y        # {nl, m}
+    for i in reversed(range(L)):
+        dW[i] = (A[i] @ dZ[i+1].T)/m  #dE/dW[i]=dE/dZ[i+1]*dZ[i+1]/dW[i]
+        db[i] = np.mean(dZ[i+1],axis=1,keepdims=True)
+        if i>0: dZ[i] = (W[i] @ dZ[i+1])*dReLU(Z[i])
     return dW, db
 
 def updateParams(W,dW,b,db,alpha):
-    for i in range(W.shape[0]):
+    for i in range(len(W)):
         W[i] -= dW[i]*alpha
         b[i] -= db[i]*alpha
-  
+
+def randomWeights(layers):
+    W = {}
+    b = {}
+    for i in range(len(layers)-1):
+        W[i] = np.random.randn(layers[i], layers[i+1])*0.01
+        b[i] = np.zeros((layers[i+1], 1))
+    return W, b
+
 def reLU(z):
     return np.maximum(0,z)
-    
+
+def dReLU(Z):
+    return (Z > 0).astype(float)
+
 def sigmoid(z):
     return 1/(1 + np.exp(-z))
     
@@ -49,13 +61,4 @@ def epoch(X,W,b,Y,alpha,runs):
     plt.title('Training Cost over Epochs')
     plt.grid(True)
     plt.show()
-
-def randomWeights(layers):
-    W = {}
-    b = {}
-    for i in range(len(nodes)-1):
-        W[i+1] = np.random.randn(layers[i], layers[i+1])*0.01
-        b[i+1] = np.zeros((layers[i+1], 1))
-    return W, b
-
     
